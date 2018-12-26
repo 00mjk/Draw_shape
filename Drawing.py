200b4a90ec0f0501bb04625d77779bb2c945b2e2
Abdalslam Aboubakar <aboubakar123678@gmail.com>
2018-12-26 08:45:16 +0000

    fixed float error message and added \n for better user interface

--- Drawing.py
+++ Drawing.py
@@ -21,8 +21,9 @@ def DrawShape( length , shape_drawn , angles_drawn ):
         else:
             abdul_turtle.left(angles_drawn)
 
-user_shape = input("Tell me the name of the shape you want to draw?\n 1)triangle\n 2)square\n 3)pentagon\n 4)hexagon\n 5)octagon\n 6)nonagon")
-user_shape_length = input("Tell me the length of each size?\n")
+user_shape = input("Tell me the name of the shape you want to draw?\n 1)triangle\n 2)square\n 3)pentagon\n 4)hexagon\n 5)octagon\n 6)nonagon\n")
+user_shape_length = float(input("Tell me the length of each size?\n"))
+#user_shape_length = float(user_shape_length)
 
 #this line finds the shape angles by checking with the map
 #uses the shape input from the user
