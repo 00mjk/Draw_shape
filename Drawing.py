b999d0903d23d3fee544218ba9603fca301ed47e
Abdalslam Aboubakar <aboubakar123678@gmail.com>
2018-12-26 12:20:22 +0000

    added the circle feature where the user can pick the number of shapes in the circle

--- Drawing.py
+++ Drawing.py
@@ -15,24 +15,33 @@ abdul_turtle.goto(0,0)
 abdul_turtle.pendown()
 abdul_turtle.speed(1)
 
-def DrawShape( length , shape_drawn , angles_drawn ):
-    for x in range(shapes[shape_drawn][0]):
-        abdul_turtle.forward(length)
-        #x doesn't need to turn at the end because it got to its final stop
-        if x == (shapes[shape_drawn][0])-1:
-            break
-        else:
-            abdul_turtle.left(angles_drawn)
+def DrawShape( length , shape_drawn , angles_drawn , numberOfShapes, anglesPerShape ):
+    for j in range(numberOfShapes):
+        for x in range(shapes[shape_drawn][0]):
+            abdul_turtle.forward(length)
+            #x doesn't need to turn at the end because it got to its final stop
+            if x == (shapes[shape_drawn][0])-1:
+                break
+            else:
+                abdul_turtle.left(angles_drawn)
+                print("AA2")
+                #added it with the old angle because it doesn't start from zero
+        abdul_turtle.left(anglesPerShape+angles4shapeBF)
+        print("AA1")
+
 
 user_shape = input("Tell me the name of the shape you want to draw?\n 1)triangle\n 2)square\n 3)pentagon\n 4)hexagon\n 5)octagon\n 6)nonagon\n")
 user_shape_length = float(input("Tell me the length of each size?\n"))
+user_shapes_in_circle = int(input("Give me the number of shapes you want me to draw in the circle?"))
 
-
+#to workout the angle depending on the number of shapes
+anglesPerShape = 360/user_shapes_in_circle
+print(anglesPerShape)
 #this line finds the shape angles by checking with the map
 #uses the shape input from the user
-angles4shape = (shapes[user_shape][1]/shapes[user_shape][0])
+angles4shapeBF = (shapes[user_shape][1]/shapes[user_shape][0])
 
 #gets the angle takes away from 180 because it starts from a stright line
-angles4shape = 180 - angles4shape
+angles4shape = 180 - angles4shapeBF
 print(angles4shape)
-DrawShape( user_shape_length , user_shape, angles4shape )
+DrawShape( user_shape_length , user_shape, angles4shape ,user_shapes_in_circle , anglesPerShape  )
