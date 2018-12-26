447b7e9ae0fd82456ff6a4f2dc13d8fd2e99f97f
Abdalslam Aboubakar <aboubakar123678@gmail.com>
2018-12-26 08:51:33 +0000

    added section to fix the bug with angles that are not 90 degree

--- Drawing.py
+++ Drawing.py
@@ -28,5 +28,8 @@ user_shape_length = float(input("Tell me the length of each size?\n"))
 #this line finds the shape angles by checking with the map
 #uses the shape input from the user
 angles4shape = (shapes[user_shape][1]/shapes[user_shape][0])
+
+#gets the angle takes away from 180 because it starts from a stright line
+angles4shape = 180 - angles4shape
 print(angles4shape)
 DrawShape( user_shape_length , user_shape, angles4shape )
